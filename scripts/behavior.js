0db88e4f162225fa5a266f841075970da41680c2
Carlos Marques <catralitos11@gmail.com>
2021-11-07 19:03:14 +0000

    Meti titulos nos graficos, mudei fontes/cores

--- scripts/behavior.js
+++ scripts/behavior.js
@@ -6,6 +6,7 @@ const main_data = 'data/movie_data.csv';
 lb_orange = "#ff8000";
 lb_green = "#00e054";
 lb_cyan = "#40bcf4";
+lb_fontColor = "#f1f3f5";
 
 
 //better to keep this as pixels, at least for now
@@ -17,6 +18,7 @@ const radius = 5;
 //I'm putting everything to change idiom sizes on top
 //vw = view width
 //vh = view height
+//Note, the ones without vw or vh it's because they get converted later down
 
 //Geo
 const geoWidth = "44vw";
@@ -29,13 +31,15 @@ const geoLabelRectX = 16
 const geoLabelRectY = "1.25vh";
 const geoLabelRectRx = "1vw";
 const geoLabelRectRy = "1vh";
-const geoLabelRectWidth= "2vh";
-const geoLabelRectHeight= "2vh";
+const geoLabelRectWidth = "2vh";
+const geoLabelRectHeight = "2vh";
 const geoLabelStep = 3;
 const geoLabelTextX = 16;
 const geoLabelTextY = "5.5vh";
 
-
+//Scatter (not vw/vh)
+const scatterWidth = 430;
+const scatterHeight = 230;
 
 
 var dataset;
@@ -81,21 +85,20 @@ Promise.all([d3.json(map_path), d3.csv(data_path), d3.csv(main_data)]).then(([ma
 
 function createGeoMap(map) {
     const cVar = 'nr_of_movies';
-    
+
     const projection = d3
         .geoMercator();
-        /*
-        .scale(height / 2)
-        .rotate([0, 0])
-        .center([0, 0])
-        .translate([geoWidth / 2, geoHeight / 2]);
-        */
-    
+    /*
+    .scale(height / 2)
+    .rotate([0, 0])
+    .center([0, 0])
+    .translate([geoWidth / 2, geoHeight / 2]);
+    */
+
     const path = d3.geoPath().projection(projection);
 
     const colorScale = d3
         .scaleLinear()
-        // ["#f7fbff","#f6faff","#f5fafe","#f5f9fe","#f4f9fe","#f3f8fe","#f2f8fd","#f2f7fd","#f1f7fd","#f0f6fd","#eff6fc","#eef5fc","#eef5fc","#edf4fc","#ecf4fb","#ebf3fb","#eaf3fb","#eaf2fb","#e9f2fa","#e8f1fa","#e7f1fa","#e7f0fa","#e6f0f9","#e5eff9","#e4eff9","#e3eef9","#e3eef8","#e2edf8","#e1edf8","#e0ecf8","#e0ecf7","#dfebf7","#deebf7","#ddeaf7","#ddeaf6","#dce9f6","#dbe9f6","#dae8f6","#d9e8f5","#d9e7f5","#d8e7f5","#d7e6f5","#d6e6f4","#d6e5f4","#d5e5f4","#d4e4f4","#d3e4f3","#d2e3f3","#d2e3f3","#d1e2f3","#d0e2f2","#cfe1f2","#cee1f2","#cde0f1","#cce0f1","#ccdff1","#cbdff1","#cadef0","#c9def0","#c8ddf0","#c7ddef","#c6dcef","#c5dcef","#c4dbee","#c3dbee","#c2daee","#c1daed","#c0d9ed","#bfd9ec","#bed8ec","#bdd8ec","#bcd7eb","#bbd7eb","#b9d6eb","#b8d5ea","#b7d5ea","#b6d4e9","#b5d4e9","#b4d3e9","#b2d3e8","#b1d2e8","#b0d1e7","#afd1e7","#add0e7","#acd0e6","#abcfe6","#a9cfe5","#a8cee5","#a7cde5","#a5cde4","#a4cce4","#a3cbe3","#a1cbe3","#a0cae3","#9ec9e2","#9dc9e2","#9cc8e1","#9ac7e1","#99c6e1","#97c6e0","#96c5e0","#94c4df","#93c3df","#91c3df","#90c2de","#8ec1de","#8dc0de","#8bc0dd","#8abfdd","#88bedc","#87bddc","#85bcdc","#84bbdb","#82bbdb","#81badb","#7fb9da","#7eb8da","#7cb7d9","#7bb6d9","#79b5d9","#78b5d8","#76b4d8","#75b3d7","#73b2d7","#72b1d7","#70b0d6","#6fafd6","#6daed5","#6caed5","#6badd5","#69acd4","#68abd4","#66aad3","#65a9d3","#63a8d2","#62a7d2","#61a7d1","#5fa6d1","#5ea5d0","#5da4d0","#5ba3d0","#5aa2cf","#59a1cf","#57a0ce","#569fce","#559ecd","#549ecd","#529dcc","#519ccc","#509bcb","#4f9acb","#4d99ca","#4c98ca","#4b97c9","#4a96c9","#4895c8","#4794c8","#4693c7","#4592c7","#4492c6","#4391c6","#4190c5","#408fc4","#3f8ec4","#3e8dc3","#3d8cc3","#3c8bc2","#3b8ac2","#3a89c1","#3988c1","#3787c0","#3686c0","#3585bf","#3484bf","#3383be","#3282bd","#3181bd","#3080bc","#2f7fbc","#2e7ebb","#2d7dbb","#2c7cba","#2b7bb9","#2a7ab9","#2979b8","#2878b8","#2777b7","#2676b6","#2574b6","#2473b5","#2372b4","#2371b4","#2270b3","#216fb3","#206eb2","#1f6db1","#1e6cb0","#1d6bb0","#1c6aaf","#1c69ae","#1b68ae","#1a67ad","#1966ac","#1865ab","#1864aa","#1763aa","#1662a9","#1561a8","#1560a7","#145fa6","#135ea5","#135da4","#125ca4","#115ba3","#115aa2","#1059a1","#1058a0","#0f579f","#0e569e","#0e559d","#0e549c","#0d539a","#0d5299","#0c5198","#0c5097","#0b4f96","#0b4e95","#0b4d93","#0b4c92","#0a4b91","#0a4a90","#0a498e","#0a488d","#09478c","#09468a","#094589","#094487","#094386","#094285","#094183","#084082","#083e80","#083d7f","#083c7d","#083b7c","#083a7a","#083979","#083877","#083776","#083674","#083573","#083471","#083370","#08326e","#08316d","#08306b"]
         .range(['#d4e4f4', '#08306b'])
         .domain(d3.extent(dataset.map((d) => +d[cVar])));
 
@@ -137,7 +140,8 @@ function createGeoMap(map) {
         .append('svg')
         .attr('id', 'legend')
         .attr('width', geoLabelWidth)
-        .attr('height', geoLabelHeight);
+        .attr('height', geoLabelHeight)
+        .attr("fill", lb_fontColor);
 
     d3.select('div#geo-label')
         .select('#legend')
@@ -145,10 +149,9 @@ function createGeoMap(map) {
         .attr('x', geoLabelX)
         .attr('y', geoLabelY)
         .text('Nª of Movies per Country:');
-    
+
     for (let i = 0; i < scale.length; i++) {
         var posX = geoLabelRectX + geoLabelStep * i;
-        //console.log(posX + "vw");
         d3.select('#legend')
             .append('rect')
             .attr('x', posX + "vw")
@@ -164,21 +167,15 @@ function createGeoMap(map) {
 
     for (let i = 0; i < scale.length; i++) {
         var posX = geoLabelTextX + geoLabelStep * i;
-        console.log(posX + "vw");
         d3.select('#legend')
             .append('text')
             .attr('x', posX + "vw")
             .attr('y', geoLabelTextY)
             .text(scale[i]);
-
     }
-
 }
 
 function createScatterPlot(data, update = false) {
-
-    const scatterWidth = 430;
-    const scatterHeight = 230;
 
     const xValue = (d) => +d[x_var];
     const yValue = (d) => +d[y_var];
@@ -195,7 +192,7 @@ function createScatterPlot(data, update = false) {
     // data = data.filter(function (d) {
     //       return d["nr_of_ratings"]=d["nr_of_ratings"]/100;  
     //   });
-
+    
     const xScale = d3
         .scaleLinear()
         .domain(d3.extent(d3.map(data, (d) => +xValue(d))))
@@ -231,7 +228,7 @@ function createScatterPlot(data, update = false) {
             .attr('id', 'x-label')
             .attr('x', scatterWidth / 2)
             .attr('y', margin.bottom - 5)
-            .attr('fill', 'black')
+            .attr('fill', lb_fontColor)
             .style('font-size', '1.3em');
 
         svg
@@ -242,13 +239,13 @@ function createScatterPlot(data, update = false) {
             .attr('id', 'y-label')
             .attr('x', -scatterHeight / 2 + margin.top + margin.bottom)
             .attr('y', -30)
-            .attr('fill', 'black')
+            .attr('fill', lb_fontColor)
             .style('font-size', '1.3em')
             .attr('transform', 'rotate(-90)');
 
         svg
             .append('g')
-            .attr('fill', 'steelblue')
+            .attr('fill', lb_cyan)
             .attr('stroke-width', 1.5)
             .attr('class', 'scatter');
     }
@@ -264,7 +261,7 @@ function createScatterPlot(data, update = false) {
     if (update === true) {
         var radius = d3
             .select('g.scatter')
-            .attr('fill', 'steelblue')
+            .attr('fill', lb_cyan)
             .attr('stroke-width', 1.5)
             .selectAll('circle')
             .remove()
@@ -272,7 +269,7 @@ function createScatterPlot(data, update = false) {
 
         radius = d3
             .select('g.scatter')
-            .attr('fill', 'steelblue')
+            .attr('fill', lb_cyan)
             .attr('stroke-width', 1.5)
             .selectAll('circle')
             .data(data)
@@ -306,7 +303,7 @@ function createScatterPlot(data, update = false) {
                         // .text((d) => { 
                         //     return d.title+"\n"+"Rating: "+d.rating+"\nRuntime: "+d.runtime;
                         // })
-                        .style("fill", "steelblue");
+                        .style("fill", lb_cyan);
 
                     // .append('title')
                     // .text((d) => { 
@@ -326,7 +323,7 @@ function createScatterPlot(data, update = false) {
     else {
         const radius = d3
             .select('g.scatter')
-            .attr('fill', 'steelblue')
+            .attr('fill', lb_cyan)
             .attr('stroke-width', 1.5)
             .selectAll('circle')
             .data(data)
@@ -360,7 +357,7 @@ function createScatterPlot(data, update = false) {
                         // .text((d) => { 
                         //     return d.title+"\n"+"Rating: "+d.rating+"\nRuntime: "+d.runtime;
                         // })
-                        .style("fill", "steelblue");
+                        .style("fill", lb_cyan);
 
                     // .append('title')
                     // .text((d) => { 
@@ -395,7 +392,7 @@ function handleMouseOver(e, d) {
 function handleMouseLeave(e, d) {
     // var name = Object.keys(d).includes('country') ? d.country : d.properties.name;
     // if (countries.includes(name)===false)
-    //     changeColor(d, 'steelblue');     
+    //     changeColor(d, lb_cyan);     
 }
 
 
@@ -407,14 +404,9 @@ function handleClick(e, d) {
     var color;
     if (Array.isArray(name) === false) {
         if (countries.includes(name) === false) {
-
-
-
             countries.push(name);
             dataChange(name);
-            changeColor(d, 'orange');
-
-
+            changeColor(d, lb_orange);
         }
         else {
             const cVar = 'nr_of_movies';
@@ -465,7 +457,7 @@ function zoomed({ transform }) {
         .attr('transform', transform);
 }
 function calculateFill(dataItem, i) {
-    return "steelblue";
+    return lb_cyan;
 }
 
 // function addCountry(country) {
